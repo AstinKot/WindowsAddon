42cf80d97b1dddf854d2e524ad0de35b7484ec01
AstinKot <murkosiks@ya.ru>
2026-01-01 14:04:42 +0300

    Update app.bat

--- app.bat
+++ app.bat
@@ -1,150 +1,164 @@
 @echo off
+title Windows Addon v1.0.1
 chcp 65001 > nul
-setlocal
+color 0a
+setlocal EnableDelayedExpansion
 
-if exist license.txt (
-    del "license.txt"
-    goto ok_license
-) else (
-    goto ok_license
-)
+:: Удаляем лицензию (если существует)
+if exist "license.txt" del /F /Q "license.txt" > nul
 
-:ok_license
-
-set "version=1.0.0"
-set "beta=yes"
-
-if "%1"=="admin" (
-    goto adminok
-) else (
-    powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/c \"\"%~f0\" admin\"' -Verb RunAs"
-    exit
-)
-
-:adminok
-set "startone=null"
-
-if exist start.txt (
-    set "startone=yes"
-    goto startone_test
-) else (
-    set "startone=no"
-    goto startone_test
-)
-
-:startone_test
-if "%startone%"=="yes" goto hello
-if "%startone%"=="no" goto menu
-call :Red "[!] Произошла ошибка при выполнении метки startone_test: неправильное значение переменной."
-pause
-exit
-
-:hello
-if "%beta%"=="yes" goto hello_beta
-if "%beta%"=="no" goto hello_not_beta
-call :Red "[!] Произошла ошибка при выполнении метки hello: неправильное значение переменной."
-pause
-exit
-
-:hello_beta
+:: Основная страница меню
+:main_menu
 cls
-call :Yellow "WindowsAddon v%version% beta"
-call :Green "[!] Добро пожаловать в WindowsAddon!"
-pause
-del "start.txt"
-goto menu
+echo ================================
+echo      Windows Addon v1.0.1      
+echo ================================
+echo 1. Настройки сети и подключения
+echo 2. Установить полезные программы
+echo 3. Параметры безопасности
+echo 4. Локализация и региональные стандарты
+echo 5. Пароль и защита учётной записи
+echo 6. Темы оформления и персонализация
+echo 7. Резервное копирование данных
+echo 8. Информация о системе
+echo 0. Выход
+echo -------------------------------
+choice /C 123456780 /N /M "Выберите действие:"
+if errorlevel 9 goto exit_app
+if errorlevel 8 goto sysinfo
+if errorlevel 7 goto backup_data
+if errorlevel 6 goto personalization
+if errorlevel 5 goto account_security
+if errorlevel 4 goto localization
+if errorlevel 3 goto security_settings
+if errorlevel 2 goto install_programs
+if errorlevel 1 goto network_settings
 
-:hello_not_beta
+:: Подменю настройки сети
+:network_settings
 cls
-call :Yellow "WindowsAddon v%version%"
-call :Green "[!] Добро пожаловать в WindowsAddon!"
-pause
-del "start.txt"
-goto menu
+echo Настройки сети и подключения
+echo ----------------------------
+echo 1. Wi-Fi подключение
+echo 2. VPN-подключение
+echo 3. Интернет-браузеры
+echo 4. Проверка скорости соединения
+echo 0. Назад
+choice /C 12340 /N /M "Выберите пункт меню:"
+if errorlevel 5 goto main_menu
+if errorlevel 4 netsh wlan show interfaces && ping google.com
+if errorlevel 3 start ms-settings:browsers
+if errorlevel 2 start ms-settings:vpn
+if errorlevel 1 start ms-settings:network-wifi
 
-:menu
-if "%beta%"=="yes" goto menu_beta
-if "%beta%"=="no" goto menu_not_beta
-call :Red "[!] Произошла ошибка при выполнении метки menu: неправильное значение переменной."
-pause
-exit
-
-:menu_beta
-set "menu_beta=null"
-
+:: Подменю установки полезных программ
+:install_programs
 cls
-call :Yellow "WindowsAddon v%version% beta"
-call :Yellow "1. Пуск"
-set /p menu_beta=Ваш выбор: 
+echo Установка полезных программ
+echo ---------------------------
+echo 1. Google Chrome
+echo 2. Microsoft Office
+echo 3. Adobe Acrobat Reader DC
+echo 4. Skype
+echo 5. Telegram Desktop
+echo 6. Steam
+echo 0. Назад
+choice /C 1234560 /N /M "Выберите приложение для установки:"
+if errorlevel 7 goto main_menu
+if errorlevel 6 start https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe
+if errorlevel 5 start https://desktop.telegram.org
+if errorlevel 4 start https://www.skype.com/en/get-skype
+if errorlevel 3 start https://get.adobe.com/reader
+if errorlevel 2 start https://office.live.com/download
+if errorlevel 1 start https://www.google.ru/chrome
 
-if "%menu_beta%"=="1" goto shutdown_menu_beta
-goto menu_beta
-
-:shutdown_menu_beta
-set "menu_shutdown_beta=null"
-
+:: Подменю безопасности
+:security_settings
 cls
-call :Yellow "WindowsAddon v%version% beta"
-call :Yellow "1. Завершение работы"
-call :Yellow "2. Завершение работы через время"
-call :Yellow "3. Перезагрузка"
-call :Yellow "4. Перезагрузка через время"
-call :Yellow "5. Выход из учётной записи"
-call :Yellow "0. Назад"
-set /p menu_shutdown_beta=Ваш выбор (0-5):
+echo Параметры безопасности
+echo ----------------------
+echo 1. Антивирус и защита от угроз
+echo 2. Брандмауэр
+echo 3. Центр обновления Windows
+echo 4. Конфиденциальность
+echo 0. Назад
+choice /C 12340 /N /M "Выберите пункт меню:"
+if errorlevel 5 goto main_menu
+if errorlevel 4 start ms-settings:privacy
+if errorlevel 3 start ms-settings:windowsupdate
+if errorlevel 2 start control /name Microsoft.WindowsFirewall
+if errorlevel 1 start ms-settings:windowsdefender
 
-if "%menu_shutdown_beta%"=="1" shutdown /s /t 0
-if "%menu_shutdown_beta%"=="2" goto shutdown_beta
-if "%menu_shutdown_beta%"=="3" shutdown /r /t 0
-if "%menu_shutdown_beta%"=="4" goto shutdown_r_beta
-if "%menu_shutdown_beta%"=="5" shutdown /l
-if "%menu_shutdown_beta%"=="0" goto menu_beta
-goto shutdown_menu_beta
+:: Подменю локализации
+:localization
+cls
+echo Локализация и региональные стандарты
+echo ----------------------------------
+echo 1. Язык интерфейса
+echo 2. Формат даты и времени
+echo 3. Клавиатура и раскладка
+echo 0. Назад
+choice /C 1230 /N /M "Выберите пункт меню:"
+if errorlevel 4 goto main_menu
+if errorlevel 3 start ms-settings:typing
+if errorlevel 2 start ms-settings:regionformat
+if errorlevel 1 start ms-settings:language
 
-:shutdown_beta
-set "sb=null"
-set /p sb=Введите через сколько секунд выключить:
+:: Подменю защиты учётной записи
+:account_security
+cls
+echo Пароль и защита учётной записи
+echo ------------------------------
+echo 1. Задать пароль для входа
+echo 2. Двухэтапная аутентификация
+echo 3. Вход в систему
+echo 0. Назад
+choice /C 1230 /N /M "Выберите пункт меню:"
+if errorlevel 4 goto main_menu
+if errorlevel 3 start ms-settings:signinoptions
+if errorlevel 2 start ms-settings:accounts
+if errorlevel 1 start net user %username% *
 
-:: Проверка на числовой ввод
-echo %sb%|findstr "^[0-9][0-9]*$">nul
-if errorlevel 1 (
-    echo Ошибка: введите число!
-    pause
-    goto shutdown_beta
-)
+:: Подменю персонализации
+:personalization
+cls
+echo Темы оформления и персонализация
+echo ---------------------------------
+echo 1. Выбор фона рабочего стола
+echo 2. Цветовые схемы
+echo 3. Экранные заставки
+echo 0. Назад
+choice /C 1230 /N /M "Выберите пункт меню:"
+if errorlevel 4 goto main_menu
+if errorlevel 3 start ms-settings:lockscreen
+if errorlevel 2 start ms-settings:colors
+if errorlevel 1 start ms-settings:background
 
-shutdown /s /t %sb%
-echo Компьютер выключится через %sb% секунд.
-pause
-goto menu_beta
+:: Подменю резервного копирования
+:backup_data
+cls
+echo Резервное копирование данных
+echo -----------------------------
+echo 1. Создать резервную копию
+echo 2. Восстановить из резервной копии
+echo 0. Назад
+choice /C 120 /N /M "Выберите пункт меню:"
+if errorlevel 3 goto main_menu
+if errorlevel 2 start ms-settings:recovery
+if errorlevel 1 start wbadmin start backup
 
-:shutdown_r_beta
-set "sr=null"
-set /p sr=Введите через сколько секунд перезагрузить:
+:: Информационная панель
+:sysinfo
+cls
+echo Информация о системе
+echo --------------------
+systeminfo
+timeout /t 10 > nul
+goto main_menu
 
-
-:: Проверка на числовой ввод
-echo %sr%|findstr "^[0-9][0-9]*$">nul
-if errorlevel 1 (
-    echo Ошибка: введите число!
-    pause
-    goto shutdown_r_beta
-)
-
-shutdown /r /t %sr%
-echo Компьютер перезагрузится через %sr% секунд.
-pause
-goto menu_beta
-
-:: Функции для цветного вывода
-:Green
-powershell -Command "Write-Host '%~1' -ForegroundColor Green"
-exit /b
-
-:Red
-powershell -Command "Write-Host '%~1' -ForegroundColor Red"
-exit /b
-
-:Yellow
-powershell -Command "Write-Host '%~1' -ForegroundColor Yellow"
+:: Выход из программы
+:exit_app
+cls
+echo Спасибо за использование Windows Addon!
+timeout /t 3 > nul
 exit /b
